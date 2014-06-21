1d80b1d976006da42d41908522d8cb3984fea177
wingnakada <wingnakada@gmail.com>
2014-06-21 12:55:13 +0900

    First Commit

---
+++ Winroad/Setup/SetupServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Winroad\Setup;
+
+use Illuminate\Support\ServiceProvider;
+
+class SetupServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('winroad/setup');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
